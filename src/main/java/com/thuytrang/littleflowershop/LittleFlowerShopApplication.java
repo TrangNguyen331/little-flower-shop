b1dd906d8bb6d2d20af5edde7de953a5276e7baf
Thùy Trang <thuytrang331@gmail.com>
2022-12-06 03:24:51 +0700

    [ 221206 ] 🌸 - start project

---
+++ src/main/java/com/thuytrang/littleflowershop/LittleFlowerShopApplication.java
@@ -0,0 +1,13 @@
+package com.thuytrang.littleflowershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleFlowerShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleFlowerShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thuytrang/littleflowershop/LittleFlowerShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thuytrang.littleflowershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleFlowerShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
