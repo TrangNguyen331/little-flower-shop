052e28d43c1e0f5b2bf1c131735819d114a12fb7
Thùy Trang <thuytrang331@gmail.com>
2022-12-06 13:52:47 +0700

    [ 221206 ] 🌸 - flower service

---
+++ src/main/java/com/thuytrang/littleflowershop/service/FlowerService.java
@@ -0,0 +1,13 @@
+package com.thuytrang.littleflowershop.service;
+
+import com.thuytrang.littleflowershop.model.Flower;
+
+import java.util.List;
+
+public interface FlowerService {
+    List<Flower> retrievalFlowers();
+    Flower saveFlower(Flower flower);
+    Flower detailFlower(Long id);
+    Flower updateFlower(Long id, Flower flower);
+    void deleteFlower(Long id);
+}
